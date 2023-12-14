bfbc479aa51ccb37e36c2da34ff465f3989d0daa
seohmoon <dream222ing@gmail.com>
2023-12-14 16:01:46 +0900

    초기설정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
-import React from 'react'; 
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='fontTest'>
-          폰트를 테스트 해볼거얌
-        </p>
-        <p className='fontTest2'>
-          폰트를 테스트 해볼거얌
-        </p>
+        <p className="fontTest">폰트를 테스트 해볼거얌</p>
+        <p className="fontTest2">폰트를 테스트 해볼거얌</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React ssak3333
         </a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 // import App from './App';
 import Namu from './namu/namu.js';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Namu />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
