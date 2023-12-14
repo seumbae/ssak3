0bceb542f41e2063612771719d08fc8613ba29db
yenalee <lmonari71@naver.com>
2023-12-14 17:42:30 +0900

    feat : calendar 틀 구축

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import '../styles/calendar.css';
 import moment from 'moment';
 
 function ReactCalendar() {
@@ -8,7 +9,7 @@ function ReactCalendar() {
 
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar onChange={onChange} value={value} next2Label={null} prev2Label={null} />
       <div className="text-gray-500 mt-4">{moment(value).format('YYYY년 MM월 DD일')}</div>
     </div>
   );
