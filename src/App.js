831e551dde7667fc7cdaa2e51ce313e9a8ad6b70
yenalee <lmonari71@naver.com>
2023-12-14 16:32:59 +0900

    feat : Page 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import RootLayout from './components/RootLayout';
 import Test from './pages/TestPage';
 import Home from './pages/Home';
 import NewFNLG from './pages/NewFNLG';
+import ChartPage from './pages/ChartPage';
+import PaymentPage from './pages/PaymentPage';
+import YourFNLG from './pages/YourFNLG';
+import BadgePage from './pages/BadgePage';
+import SavingsPage from './pages/SavingsPage';
+import FriendPage from './pages/FriendPage';
 import './App.css';
 
 function App() {
@@ -20,6 +26,12 @@ function App() {
         <Route path="/" element={<Test />} />
         <Route path="/home" element={<Home />} />
         <Route path="/new" element={<NewFNLG />} />
+        <Route path="/chart" element={<ChartPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/yours" element={<YourFNLG />} />
+        <Route path="/badge" element={<BadgePage />} />
+        <Route path="/savings" element={<SavingsPage />} />
+        <Route path="/friend" element={<FriendPage />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/PaymentPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PaymentPage() {
+  return <div>결제내역 화면 입니다.</div>;
+}
+
+export default PaymentPage;
