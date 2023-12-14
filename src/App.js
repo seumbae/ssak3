75eb0231eb3c7f444001bb4fd9915f9c24920968
seumbae <ohcm0013@gmail.com>
2023-12-14 15:57:02 +0900

    update: Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'; 
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import RootLayout from './components/RootLayout';
+import Test from './pages/TestPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='fontTest'>
-          폰트를 테스트 해볼거얌
-        </p>
-        <p className='fontTest2'>
-          폰트를 테스트 해볼거얌
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React ssak3333
-        </a>
-      </header>
-    </div>
-  );
+    /**
+     * path="/" 아래에 작성하세용
+     * 
+     * 아래와 같이 자신이 만든 페이지 연결하시면 됩니다
+		 * <Route path="/login" element={<LoginPage />}></Route>
+     * <Route path="" element={<Navigate to="/login" />} />
+     */
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route path="/" element={<Test />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
