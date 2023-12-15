bf2acafc67c00305fba76d2fd2e74d379f2481ca
seumbae <ohcm0013@gmail.com>
2023-12-15 17:44:20 +0900

    feat : 버튼 눌렸을 때

---
+++ src/components/CheckedBtn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import checkedBtn from '../assets/images/cButton.png';
+
+function CheckedBtn() {
+    return <img src={checkedBtn} alt="checkedBtn"  />;
+}
+
+export default CheckedBtn;
