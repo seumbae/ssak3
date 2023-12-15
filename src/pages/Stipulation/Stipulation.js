c743e3f8e1a49c39e39f9fe42fd15d46aa7e95ed
seumbae <ohcm0013@gmail.com>
2023-12-15 14:51:30 +0900

    feat : 약관동의 페이지 생성 중

--- src/pages/Stipulation/Stipulation.js
+++ src/pages/Stipulation/Stipulation.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Step1 from '../../assets/images/stipulationStep1.png';
+import NextBtnComplete from '../../components/NextBtnComplete';
+import '../../styles/stipulation.css';
 
 function Stipulation(props) {
   const navigate = useNavigate();
   const location = useLocation();
-
+  console.log(location);
   if (location.state.idx === 1) {
     return (
-      <div>
-        <img src={Step1} alt="step1" />
-        <div>돈기브업서비스 가입을 시작합니다</div>
+      <div className="body-relative flex">
+        <div className="body-container">
+          <img src={Step1} alt="step1" />
+          <div className='fs-4 fw-bold mt-5' >돈기브업서비스 가입을 시작합니다</div>
+          <div>
+            <div>
+              <div>약관 전체동의</div>
+            </div>
+            {/* 가로줄 */}
+            <div>
+              {/* button */}
+              <div>돈기브업서비스 이용약관[필수]</div>
+            </div>
+            <div>
+              {/* button */}
+              <div>마이데이터 관련 사용 동의서[필수]</div>
+            </div>
+            <div>
+              {/* button */}
+              <div>돈기브업서비스 설명서[필수]</div>
+            </div>
+            <div>
+              {/* button */}
+              <div>돈기브업서비스 설명서[필수]</div>
+            </div>
+          </div>
+        </div>
         <div>
-          <div>약관 전체동의</div>
+          {/* 다음 버튼 */}
+          <NextBtnComplete>다음</NextBtnComplete>
         </div>
       </div>
     );
