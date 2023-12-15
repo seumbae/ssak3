6a51a2e7ad3dc0407e56345d79d107460fc73ae4
kahyu <kahyu1597@gmail.com>
2023-12-15 09:25:50 +0900

    add predictMotive component, edit userBar component;

--- src/components/UserBar.js
+++ src/components/UserBar.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import '../styles//UserBar.css'
-import iconAvatar from '../assets/iconAvatar.png'
 import iconAlarmBell from '../assets/iconAlarmBell.png'
 import iconSettings from '../assets/iconSettings.png'
 
-function UserBar() {
+function UserBar(Props) {
   return (
     <div className='UserBar'>
       <div className='greetingContainer'>
-        <img className='avatar' src={iconAvatar} alt="이미지" />
-        <p className='greetingText'>슴배님, 안녕하세요</p>
+        <img className='avatar' src={Props.userAvatar} alt="이미지" />
+        <p className='greetingText'>{Props.userName}님, 안녕하세요</p>
       </div>
       <div className='imgContainer'>
         <img className='alarmBell' src={iconAlarmBell} alt="이미지" />
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import UserBar from "../components/UserBar";
 import ServiceList from "../components/ServiceList"
+import PredictMotive from "../components/PredictMotive"
+import iconAvatar from '../assets/iconAvatar.png'
 
 function Test() {
     return (
         <div>
             <div>    .    </div>
-            <UserBar />
+            <UserBar userName='슴배' userAvatar={iconAvatar} />
+            <div>    .    </div>
+            <PredictMotive saveMoney='3,000' />
             <div>    .    </div>
             <ServiceList />
             <div>    .    </div>
