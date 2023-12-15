a29ddcc2ecd5949bd3b8c52bb67d48763459808a
seumbae <ohcm0013@gmail.com>
2023-12-15 17:43:44 +0900

    feat : 모든 체크버튼 눌렸을 때

--- src/components/NextBtnComplete.js
+++ src/components/NextBtnComplete.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "../styles/common.css";
+import React from 'react';
+import '../styles/common.css';
 
-function NextBtnComplete({children}) {
-    return (
-        <div className="next-btn-complete">{children}</div>
-    )
+function NextBtnComplete({ children, handleNextClick}) {
+  return (
+    <button className="next-btn-complete fs-2" onClick={handleNextClick}>
+      <div>{children}</div>
+    </button>
+  );
 }
 
 export default NextBtnComplete;
