2a36c878afcf4374f554f94eeea527aa6ccb4471
seumbae <ohcm0013@gmail.com>
2023-12-15 14:51:49 +0900

    Btn

---
+++ src/components/NextBtnComplete.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/common.css";
+
+function NextBtnComplete({children}) {
+    return (
+        <div className="next-btn-complete">{children}</div>
+    )
+}
+
+export default NextBtnComplete;
