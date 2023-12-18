2395384f2ce271f086361d214732e058110c34ff
yenalee <lmonari71@naver.com>
2023-12-18 11:14:22 +0900

    design : calendar css 수정

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -25,23 +25,23 @@ function ReactCalendar() {
 
   return (
     <div>
-      <div className="toggle-container flex">
+      <div className="toggle-container">
         <input type="checkbox" className="btn-check" id="btn-check-outlined1"></input>
-        <label className="whole-btn btn btn-outline-primary" htmlFor="btn-check-outlined1">
+        <label className="whole-btn" htmlFor="btn-check-outlined1">
           전체
         </label>
         <div className="divider"></div>
         <input type="checkbox" className="btn-check btn-check2" id="btn-check-outlined2"></input>
-        <label className="income-btn btn btn-outline-primary" htmlFor="btn-check-outlined2">
+        <label className="income-btn " htmlFor="btn-check-outlined2">
           수입
         </label>
         <input type="checkbox" className="btn-check btn-check2" id="btn-check-outlined3"></input>
-        <label className="income-btn btn btn-outline-primary" htmlFor="btn-check-outlined3">
+        <label className="income-btn " htmlFor="btn-check-outlined3">
           지출
         </label>
       </div>
       <div className="triangle"></div>
-      <div className="category-container flex">
+      <div className="category-container">
         <div className="cat-btn cat-1">전체</div>
         <div className="cat-btn cat-2">교통</div>
         <div className="cat-btn cat-3">쇼핑</div>
@@ -66,12 +66,13 @@ function ReactCalendar() {
           value={value}
           tileContent={addContent}
           showNeighboringMonth={true}
-          // next2Label={null}
-          // prev2Label={null}
+          next2Label={null}
+          prev2Label={null}
           oneWeekCalendar={true}
         />
       </div>
-      <div id="outerContainer">
+      {/* 월별 주별 토글버튼 */}
+      {/* <div id="outerContainer">
         <div className="select-left" id="container">
           <div id="item"></div>
           <div className="left">
@@ -81,8 +82,8 @@ function ReactCalendar() {
             <span>주별</span>
           </div>
         </div>
-      </div>
-      <div className="container">
+      </div> */}
+      <div className="title-container">
         <div className="record-main-title mt-4 row">
           <div className="col-sm"></div>
           <div className="today col-sm">{moment(value).format('YYYY.MM.DD')}</div>
@@ -135,158 +136,159 @@ function ReactCalendar() {
   );
 }
 
-window.onload = function () {
-  var dragItem = document.querySelector('#item');
-  var container = document.querySelector('#container');
+// 월별주별 토글
+// window.onload = function () {
+//   var dragItem = document.querySelector('#item');
+//   var container = document.querySelector('#container');
 
-  console.log('container', container);
-  var active = false;
-  var currentX;
-  var initialX;
-  var itemClick;
-  var xOffset = 0;
+//   console.log('container', container);
+//   var active = false;
+//   var currentX;
+//   var initialX;
+//   var itemClick;
+//   var xOffset = 0;
 
-  function isFirefox() {
-    return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-  }
+//   function isFirefox() {
+//     return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+//   }
 
-  function dragStart(e) {
-    var xPos = e.pageX - this.getBoundingClientRect().left;
+//   function dragStart(e) {
+//     var xPos = e.pageX - this.getBoundingClientRect().left;
 
-    if (e.type === 'touchstart') {
-      var xPosMobile = e.touches[0].pageX - this.getBoundingClientRect().left;
-      initialX = xPosMobile;
-    } else {
-      initialX = xPos;
-    }
+//     if (e.type === 'touchstart') {
+//       var xPosMobile = e.touches[0].pageX - this.getBoundingClientRect().left;
+//       initialX = xPosMobile;
+//     } else {
+//       initialX = xPos;
+//     }
 
-    dragItem.style.transition = 'all .2s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
+//     dragItem.style.transition = 'all .2s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
 
-    if (e.target === dragItem) {
-      active = true;
-    }
-  }
+//     if (e.target === dragItem) {
+//       active = true;
+//     }
+//   }
 
-  function itemDragStart(e) {
-    var xPos = e.pageX - this.getBoundingClientRect().left;
+//   function itemDragStart(e) {
+//     var xPos = e.pageX - this.getBoundingClientRect().left;
 
-    itemClick = xPos;
-  }
+//     itemClick = xPos;
+//   }
 
-  function toggleSwitch() {
-    if (initialX > 100) {
-      currentX = 0;
-    } else {
-      currentX = 200;
-    }
-  }
+//   function toggleSwitch() {
+//     if (initialX > 100) {
+//       currentX = 0;
+//     } else {
+//       currentX = 200;
+//     }
+//   }
 
-  function dragEnd(e) {
-    initialX = currentX;
-    active = false;
+//   function dragEnd(e) {
+//     initialX = currentX;
+//     active = false;
 
-    if (initialX > 100) {
-      currentX = 36;
-      dragItem.style.transition = 'all .2s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
-      container.classList.add('select-right');
-      container.classList.remove('select-left');
-    } else {
-      currentX = 0;
-      dragItem.style.transition = 'all .2s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
-      container.classList.remove('select-right');
-      container.classList.add('select-left');
-    }
+//     if (initialX > 100) {
+//       currentX = 36;
+//       dragItem.style.transition = 'all .2s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
+//       container.classList.add('select-right');
+//       container.classList.remove('select-left');
+//     } else {
+//       currentX = 0;
+//       dragItem.style.transition = 'all .2s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
+//       container.classList.remove('select-right');
+//       container.classList.add('select-left');
+//     }
 
-    setTranslate(currentX, dragItem);
-  }
+//     setTranslate(currentX, dragItem);
+//   }
 
-  function drag(e) {
-    var xPos = e.pageX - this.getBoundingClientRect().left;
+//   function drag(e) {
+//     var xPos = e.pageX - this.getBoundingClientRect().left;
 
-    if (!(xPos > 400 || xPos < 0)) {
-      if (active) {
-        e.preventDefault();
+//     if (!(xPos > 400 || xPos < 0)) {
+//       if (active) {
+//         e.preventDefault();
 
-        if (e.type === 'touchmove') {
-          var xPosMobile = e.touches[0].pageX - this.getBoundingClientRect().left;
-          currentX = xPosMobile - initialX;
-          if (initialX > 200) {
-            currentX = xPosMobile - itemClick;
-          }
-          if (currentX > 200) {
-            currentX = 200;
-            active = false;
-            container.classList.add('select-right');
-            container.classList.remove('select-left');
-          } else if (currentX < 0) {
-            currentX = 0;
-            active = false;
-            container.classList.remove('select-right');
-            container.classList.add('select-left');
-          }
-        } else {
-          currentX = xPos - initialX;
-          if (initialX > 200) {
-            currentX = xPos - itemClick;
-          }
-          if (currentX > 200) {
-            currentX = 200;
-            active = false;
-            container.classList.add('select-right');
-            container.classList.remove('select-left');
-          } else if (currentX < 0) {
-            currentX = 0;
-            active = false;
-            container.classList.remove('select-right');
-            container.classList.add('select-left');
-          }
-        }
+//         if (e.type === 'touchmove') {
+//           var xPosMobile = e.touches[0].pageX - this.getBoundingClientRect().left;
+//           currentX = xPosMobile - initialX;
+//           if (initialX > 200) {
+//             currentX = xPosMobile - itemClick;
+//           }
+//           if (currentX > 200) {
+//             currentX = 200;
+//             active = false;
+//             container.classList.add('select-right');
+//             container.classList.remove('select-left');
+//           } else if (currentX < 0) {
+//             currentX = 0;
+//             active = false;
+//             container.classList.remove('select-right');
+//             container.classList.add('select-left');
+//           }
+//         } else {
+//           currentX = xPos - initialX;
+//           if (initialX > 200) {
+//             currentX = xPos - itemClick;
+//           }
+//           if (currentX > 200) {
+//             currentX = 200;
+//             active = false;
+//             container.classList.add('select-right');
+//             container.classList.remove('select-left');
+//           } else if (currentX < 0) {
+//             currentX = 0;
+//             active = false;
+//             container.classList.remove('select-right');
+//             container.classList.add('select-left');
+//           }
+//         }
 
-        dragItem.style.transition = 'all .05s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
+//         dragItem.style.transition = 'all .05s cubic-bezier(0.04, 0.46, 0.36, 0.99)';
 
-        xOffset = currentX;
+//         xOffset = currentX;
 
-        setTranslate(currentX, dragItem);
-      }
-    } else {
-      active = false;
+//         setTranslate(currentX, dragItem);
+//       }
+//     } else {
+//       active = false;
 
-      if (initialX > 200) {
-        dragItem.style.transform = 'translate3d(200px, 0px, 0)';
-      } else {
-        dragItem.style.transform = 'translate3d(0, 0px, 0)';
-      }
-    }
-  }
+//       if (initialX > 200) {
+//         dragItem.style.transform = 'translate3d(200px, 0px, 0)';
+//       } else {
+//         dragItem.style.transform = 'translate3d(0, 0px, 0)';
+//       }
+//     }
+//   }
 
-  function setTranslate(xPos, el) {
-    el.style.transform = 'translate3d(' + xPos + 'px, 0px, 0)';
-  }
-  // window.onload = function () {
-  if (isFirefox()) {
-    container.addEventListener('mouseup', dragEnd, false);
-    container.addEventListener('click', toggleSwitch, false);
-  } else {
-    console.log('container 생성');
-    // if (container) {
-    // console.log('container 생성2');
-    // container.addEventListener('touchstart', dragStart, false);
-    // container.addEventListener('touchend', dragEnd, false);
-    // container.addEventListener('touchmove', drag, false);
+//   function setTranslate(xPos, el) {
+//     el.style.transform = 'translate3d(' + xPos + 'px, 0px, 0)';
+//   }
+//   // window.onload = function () {
+//   if (isFirefox()) {
+//     container.addEventListener('mouseup', dragEnd, false);
+//     container.addEventListener('click', toggleSwitch, false);
+//   } else {
+//     console.log('container 생성');
+//     // if (container) {
+//     // console.log('container 생성2');
+//     // container.addEventListener('touchstart', dragStart, false);
+//     // container.addEventListener('touchend', dragEnd, false);
+//     // container.addEventListener('touchmove', drag, false);
 
-    // container.addEventListener('mousedown', dragStart, false);
-    // dragItem.addEventListener('mousedown', itemDragStart, false);
+//     // container.addEventListener('mousedown', dragStart, false);
+//     // dragItem.addEventListener('mousedown', itemDragStart, false);
 
-    // container.addEventListener('mousemove', drag, false);
+//     // container.addEventListener('mousemove', drag, false);
 
-    container.addEventListener('mouseup', dragEnd, false);
-    container.addEventListener('click', toggleSwitch, false);
-  }
-  // }
-  // };
+//     container.addEventListener('mouseup', dragEnd, false);
+//     container.addEventListener('click', toggleSwitch, false);
+//   }
+//   // }
+//   // };
 
-  // Rest of your code that uses addEventListener goes here
-};
+//   // Rest of your code that uses addEventListener goes here
+// };
 
 // var dragItem = document.querySelector('#item');
 // var container = document.querySelector('#container');
