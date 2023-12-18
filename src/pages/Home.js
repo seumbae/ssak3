6f27bdbddf3b6a8ba3512c77f6941a7e08726f22
kahyu <kahyu1597@gmail.com>
2023-12-18 15:02:11 +0900

    move components to Home from test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,32 @@
+import React, { useState } from "react";
 import ReactCalendar from '../components/Calendar.js';
-import React from 'react';
+import UserBar from "../components/UserBar";
+import FNLGBar from "../components/FNLGBar"
+import BudgetBar from "../components/BudgetBar";
+import PredictMotive from "../components/PredictMotive"
+import ServiceList from "../components/ServiceList"
+import iconAvatar from '../assets/images/iconAvatar.png'
 
 function Home() {
+  const use = 500000;
+  const budget = 1000000;
+  const saveMoney = 3000;
+  const [FNLG, setFNLG] = useState('식비');
+
   return (
-    <div>
-      <ReactCalendar></ReactCalendar>
-    </div>
+    <div className="paddingBox">
+            <div className="emptyBox"></div>
+            <UserBar userName='슴배' userAvatar={iconAvatar} />
+            <div className="emptyBox"></div>
+            <FNLGBar FNLGList={["전체", "식비", "패션/쇼핑", "카페/간식", "교통/자동차", "취미/여가"]} getFNLG={(goal) => setFNLG(goal)} defaultGoal={FNLG} />
+            <div className="emptyBox"></div>
+            <BudgetBar use={use} budget={budget} />
+            <div className="emptyBox"></div>
+            <ReactCalendar />
+            <div className="emptyBox"></div>
+            <PredictMotive saveMoney={saveMoney} />
+            <ServiceList />
+        </div>
   );
 }
 
