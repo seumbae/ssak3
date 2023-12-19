87f7d7185976f9539509548e1235d4c672bcf53c
kahyu <kahyu1597@gmail.com>
2023-12-19 14:05:13 +0900

    merge conflict

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,14 @@ import PredictMotive from '../components/PredictMotive';
 import ServiceList from '../components/ServiceList';
 import iconAvatar from '../assets/images/iconAvatar.png';
 
+import { getUsers } from '../services/service';
+
 function Home() {
   const use = 500000;
   const budget = 1000000;
   const saveMoney = 3000;
   const [FNLG, setFNLG] = useState('식비');
 
-<<<<<<< HEAD
-=======
   try {
     getUsers(localStorage.getItem('userId')).then(({data, status}) => {
       if (status === 200) {
@@ -33,21 +33,20 @@ function Home() {
     alert('서버와의 연결이 원활하지 않습니다.');
   }
 
->>>>>>> 2a8163cf9bd96dfdb984ba6328fb3a95e9764ff8
   return (
     <div className="paddingBox">
-      <div className="emptyBox"></div>
-      <UserBar userName={localStorage.getItem('userName')} userAvatar={iconAvatar} />
-      <div className="emptyBox"></div>
-      <FNLGBar FNLGList={["전체", "식비", "패션/쇼핑", "카페/간식", "교통/자동차", "취미/여가"]} getFNLG={(goal) => setFNLG(goal)} defaultGoal={FNLG} />
-      <div className="emptyBox"></div>
-      <BudgetBar use={use} budget={budget} />
-      <div className="emptyBox"></div>
-      <ReactCalendar />
-      <div className="emptyBox"></div>
-      <PredictMotive saveMoney={saveMoney} />
-      <ServiceList />
-    </div>
+            <div className="emptyBox"></div>
+            <UserBar userName={localStorage.getItem('userName')} userAvatar={iconAvatar} />
+            <div className="emptyBox"></div>
+            <FNLGBar FNLGList={["전체", "식비", "패션/쇼핑", "카페/간식", "교통/자동차", "취미/여가"]} getFNLG={(goal) => setFNLG(goal)} defaultGoal={FNLG} />
+            <div className="emptyBox"></div>
+            <BudgetBar use={use} budget={budget} />
+            <div className="emptyBox"></div>
+            <ReactCalendar />
+            <div className="emptyBox"></div>
+            <PredictMotive saveMoney={saveMoney} />
+            <ServiceList />
+        </div>
   );
 }
 
