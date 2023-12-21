bf7d0718a6d951597b7565b25e82ec33f02bc635
yenalee <lmonari71@naver.com>
2023-12-21 14:50:08 +0900

    feat : badgepage 복원

--- src/pages/BadgePage.js
+++ src/pages/BadgePage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import tree from '../assets/images/tree.png';
+import badge from '../assets/images/badge.png';
 import '../styles/badge.css';
 function BadgePage() {
   return (
-    <>
-      <div className="badge-container">
-        <img className="treeImg mt-3" src={tree} alt="tree" />
-      </div>
-      <div className="badge-save-container">
-        <h2>배지 보관함</h2>
-        <h4>획득한 배지를 클릭하여 트리를 꾸며보세요!</h4>
-      </div>
-    </>
+    <div className="body-container">
+      <img src={badge} alt="chart" />
+    </div>
+    // {/* <div className="badge-container">
+    //   <img className="treeImg mt-3" src={tree} alt="tree" />
+    // </div>
+    // <div className="badge-save-container">
+    //   <h2>배지 보관함</h2>
+    //   <h4>획득한 배지를 클릭하여 트리를 꾸며보세요!</h4>
+    // </div> */}
   );
 }
 
