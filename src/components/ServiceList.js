80078edc7042c1285b92f029cf0abafa18fb3d9a
yenalee <lmonari71@naver.com>
2023-12-22 16:41:20 +0900

    chore : 서비스리스트 수정

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 import ServiceItem from './ServiceItem';
-import iconStat from '../assets/images/iconStat.png'
-import iconYourFNLG from '../assets/images/iconYourFNLG.png'
-import iconPaymentRecord from '../assets/images/iconPaymentRecord.png'
-import iconBadge from '../assets/images/iconBadge.png'
-import iconAd from '../assets/images/iconAd.png'
-import iconFriend from '../assets/images/iconFriend.png'
+import iconStat from '../assets/images/iconStat.png';
+import iconYourFNLG from '../assets/images/iconYourFNLG.png';
+import iconPaymentRecord from '../assets/images/iconPaymentRecord.png';
+import iconBadge from '../assets/images/iconBadge.png';
+import iconAd from '../assets/images/iconAd.png';
+import iconFriend from '../assets/images/iconFriend.png';
 import { useNavigate } from 'react-router-dom';
 
 function ServiceList() {
@@ -25,7 +25,7 @@ function ServiceList() {
       case '활동 배지':
         navigate('/badge');
         break;
-      case '적금':
+      case '상품':
         navigate('/savings');
         break;
       case '친구':
@@ -37,16 +37,52 @@ function ServiceList() {
   return (
     <div>
       <ListBox>
-        <ServiceItem handleCardClick={handleCardClick} title='통계' desTop='나의 모든 소비 내역을' desBottom='자세히 보여주는' image={iconStat} />
-        <ServiceItem handleCardClick={handleCardClick} title='너의 가계부' desTop='다른 이의 좋은 정보를' desBottom='쏙쏙 얻을 수 있는' image={iconYourFNLG} />
+        <ServiceItem
+          handleCardClick={handleCardClick}
+          title="통계"
+          desTop="테마와 관련된 다양한"
+          desBottom="데이터 기반"
+          image={iconStat}
+        />
+        <ServiceItem
+          handleCardClick={handleCardClick}
+          title="너의 가계부"
+          desTop="다른 이의 좋은 정보를"
+          desBottom="쏙쏙 얻을 수 있는"
+          image={iconYourFNLG}
+        />
       </ListBox>
       <ListBox>
-        <ServiceItem handleCardClick={handleCardClick} title='결제내역' desTop='마이데이터와 연계된' desBottom='나의' image={iconPaymentRecord} />
-        <ServiceItem handleCardClick={handleCardClick} title='활동 배지' desTop='내 절약생활의' desBottom='발자취' image={iconBadge} />
+        <ServiceItem
+          handleCardClick={handleCardClick}
+          title="결제내역"
+          desTop="마이데이터와 연계된"
+          desBottom="나의"
+          image={iconPaymentRecord}
+        />
+        <ServiceItem
+          handleCardClick={handleCardClick}
+          title="활동 배지"
+          desTop="내 절약생활의"
+          desBottom="발자취"
+          image={iconBadge}
+        />
       </ListBox>
       <ListBox>
-        <ServiceItem handleCardClick={handleCardClick} title='적금' desTop='아껴서 모은 돈으로' desBottom='새로운 저축의 시작' image={iconAd} />
-        <ServiceItem handleCardClick={handleCardClick} title='친구' desTop='공동의 목표를 향한' desBottom='한걸음' image={iconFriend} />
+        <ServiceItem
+          handleCardClick={handleCardClick}
+          title="상품"
+          desTop="아껴서 모은 돈으로"
+          desBottom="새로운 저축의 시작"
+          image={iconAd}
+        />
+        <ServiceItem
+          handleCardClick={handleCardClick}
+          title="친구"
+          desTop="공동의 목표를 향한"
+          desBottom="한걸음"
+          image={iconFriend}
+        />
       </ListBox>
     </div>
   );
@@ -56,7 +92,6 @@ const ListBox = styled.div`
   display: flex;
   justify-content: space-between;
   padding-top: 10px;
-`
+`;
 
 export default ServiceList;
- 
