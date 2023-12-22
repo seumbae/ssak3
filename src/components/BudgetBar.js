b76aefb0d859de77766e928dc44f49ec5da24b03
yenalee <lmonari71@naver.com>
2023-12-22 17:47:05 +0900

    style : pg bar 색 수정

--- src/components/BudgetBar.js
+++ src/components/BudgetBar.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import '../styles//BudgetBar.css'
+import '../styles//BudgetBar.css';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function BudgetBar(props) {
   // document.documentElement.style.setProperty("--progWidth", props.use/props.budget*100+"%");
   return (
-    <div className='BudgetBar'>
-      <div className='leftBox'>
-        목표<br />예산
+    <div className="BudgetBar">
+      <div className="leftBox">
+        목표
+        <br />
+        예산
       </div>
-      <div className='rightBox'>
-        <div className='goal'>{props.curledger.goal}</div>
-        <div className='progStat'>{(props.budget - props.use).toLocaleString()} / {props.budget.toLocaleString()}</div>
-        <div className='progBar'>
+      <div className="rightBox">
+        <div className="goal">{props.curledger.goal}</div>
+        <div className="progStat">
+          {(props.budget - props.use).toLocaleString()} / {props.budget.toLocaleString()}
+        </div>
+        <div className="progBar">
           {/* <div className='prog'></div> */}
-          <ProgressBar animated variant="danger" now={(props.budget - props.use) / props.budget * 100} />;
+          <ProgressBar animated variant="warning" now={((props.budget - props.use) / props.budget) * 100} />;
         </div>
       </div>
     </div>
