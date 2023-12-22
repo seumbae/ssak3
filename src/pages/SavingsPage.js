1283b4cf35a84ed1a4d922e5dc194ec6be67e9d8
yenalee <lmonari71@naver.com>
2023-12-22 17:35:52 +0900

    feat : new page 연결

--- src/pages/SavingsPage.js
+++ src/pages/SavingsPage.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import saving from '../assets/images/saving.png';
+import saving from '../assets/images/productPage.png';
 
 function SavingsPage() {
   return (
     <div className="body-container">
-      <img src={saving} alt="chart" />
+      <img style={{ width: '100%' }} src={saving} alt="chart" />
     </div>
   );
 }
