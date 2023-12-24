b7096888df9b744b252a3a83e9d5e01e4cf31d91
seumbae <ohcm0013@gmail.com>
2023-12-24 12:53:09 +0900

    feat: 글씨 흐림 이슈 해결

--- src/pages/SavingsPage.js
+++ src/pages/SavingsPage.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
 import saving from '../assets/images/productPage.png';
+import styles from '../styles/saving.module.css';
 
 function SavingsPage() {
   return (
     <div className="body-container">
+      <div className={styles.wrapper}>
+        <div className={styles.h1}>상품 안내</div>
+        <div className={styles.h2}>나에게 맞는 상품을 더 알아보아요</div>
+      </div>
       <img style={{ width: '100%' }} src={saving} alt="chart" />
     </div>
   );
